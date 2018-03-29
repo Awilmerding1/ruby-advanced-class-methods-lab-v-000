51f992d3f138de9cfbd107b63b2673808a909c21
Annabel Wilmerding <annabelwilmerding@gmail.com>
2018-03-29 15:23:59 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -15,8 +15,7 @@ class Song
   end
   
   def self.new_by_name(song_name)
-    @name = song_name
-    self.new.tap{|song| @name = song.name}
+    self.new.tap{|song| song.name}
   end
   
   def self.create_by_name(song_name)
