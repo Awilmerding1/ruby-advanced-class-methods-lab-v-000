deb316b57fdac3803be99268893dca9aca834756
Annabel Wilmerding <annabelwilmerding@gmail.com>
2018-03-29 15:02:59 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -11,23 +11,15 @@ class Song
   end
   
   def self.create
-  self.all << self.new
-  self.all[-1]
+    self.new.tap{|song| song.save}
   end
   
   def self.new_by_name(song_name)
-    new_song = self.new 
-    @name = song_name
-    new_song.name = @name
-    return new_song
+    self.new.tap{|song| @name = song_name}
   end
   
   def self.create_by_name(song_name)
-    @name = song_name
-    new_song = self.new 
-    new_song.name = @name
-    self.all << new_song
-    @@all[-1]
+    self.new_by_name(song_name).tap{|song| song.save}
   end
   
   def self.find_by_name(name)
@@ -39,23 +31,13 @@ class Song
   def self.find_or_create_by_name(name)
     if self.find_by_name(name)
       self.find_by_name(name)
-    else self.create_by_name(name)
+    else 
+      self.create_by_name(name)
     end
   end
   
   def self.alphabetical
-    array = []
-    array_two = []
-    self.all.map do |song|
-    array << song.name
-    end
-    sorted = array.sort do |a, b|
-      a <=> b 
-    end 
-    sorted.map do |name|
-     array_two << find_by_name(name)
-  end
-  array_two
+    @@all.sort_by{|song| song.name}
 end
   
   def self.new_from_filename(file_name)
